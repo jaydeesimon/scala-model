bb4cbb87d1d5b77d6cc52ec02437edeb3ef607b4
Jeffrey Simon <jdsimon@gmail.com>
2014-04-15 23:37:08 -0400

    Created grid

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,3 +1,11 @@
-object MainApp extends App {
-  println("I am a seed app")
+import java.awt.Dimension
+import scala.swing._
+
+object MainApp extends SimpleSwingApplication {
+
+  override def top: Frame = new MainFrame {
+    preferredSize = new Dimension(Playfield.StandardSize.width, Playfield.StandardSize.height + 6)
+    contents = new Playfield
+    centerOnScreen()
+  }
 }
